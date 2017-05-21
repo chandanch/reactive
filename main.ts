9be39c0f2a2c16c2051f062c5ff04a4cdabddbdf
3E Software Solutions <chandanb@3esofttech.com>
2017-05-21 19:53:40 +0530

    code clean up for part 2

--- main.ts
+++ main.ts
@@ -1,18 +1,25 @@
-import { Observable, Observer } from "rxjs";
+/**
+* @desc Creates an Observable and Observer 
+*/
+
+import { Observable } from "rxjs";
 
 var scores = [12,34,5,6,7];
 
-// var source = Observable.from(scores);
+// Pass a function with observer object as a parameter to create method
 let source = Observable.create(observer => {
+    // Iterate through the scores and send the each score to the data stream using next()
     for(let score of scores) {
-        if (score === 5) {
-            observer.error("Something went wrong");
-        }
+        // if (score === 5) {
+        //     observer.error("Something went wrong");
+        // }
         observer.next(score);
     }
+    // once all the data has been sent through data stream
     observer.complete();
 });
 
+// an observer which subscribe to the observable
 source.subscribe(
     data => {
         console.log("Data Recieved:", data);
