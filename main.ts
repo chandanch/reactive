05fe4e6a86c9952d275403579346e050cbe917d8
chandanch <chandan.cec107@gmail.com>
2017-05-29 13:01:59 +0530

    code cleanup with comments

--- main.ts
+++ main.ts
@@ -4,15 +4,26 @@
 */
 import { Observable } from "rxjs";
 
+
 let circle = document.getElementById('circle');
+// 1. Create an observable using the fromEvent
+// 2. Pass the document and movemove as the event to the fromEvent
 let source = Observable.fromEvent(document, "mousemove")
+    /* 3. Process the data recieved from the mouse event, since the mouse event 
+    * returns many properties we capture and process only the clientx and clienty
+    */
     .map((e: MouseEvent) => {
+        // 4.For every mouse event 'e' pass the event to the function which will return an object
         return {
+            // get the clientX property
             x: e.clientX,
+            // get the clientY property
             y: e.clientY
         }
     })
+    // Emit the events for which the clientX is less than 1000
     .filter(value => value.x < 1000)
+    // Using the temporal operator send the data to observer after 300ms delay
     .delay(300);
 
 function onNext(value) {
@@ -21,7 +32,8 @@ function onNext(value) {
 }
 // an observer which subscribe to the observable
 source.subscribe(
-    onNext,
+    //the onNext is used to handle the data values recieved
+    onNext, 
     error => {
         console.log(`Error: ${error}`)
     },
