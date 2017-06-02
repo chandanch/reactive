71c94934545e4ad8b3bd16792dea199a9533d367
chandanch <chandan.cec107@gmail.com>
2017-06-02 15:26:06 +0530

    added comments

--- main.ts
+++ main.ts
@@ -7,25 +7,37 @@ import { Observable } from "rxjs";
 
 let button = document.getElementById('getMoviesBtn');
 let output = document.getElementById('moviesContainer');
-// 1. Create an observable using the fromEvent
+// Create an observable using the fromEvent
 
+// observable to handle button click 
 let source = Observable.fromEvent(button, "click");
 
+/**
+ * @desc Get the data by making HTTP request
+ * @param url
+ */
 function getMovies(url : string) {
+    // Create an XHMLHTTPRequest object
     let xhr = new XMLHttpRequest();
 
+    // event handler for handling the response from the server
     xhr.addEventListener("load", () => {
+        // parse the response and add to the movies
         let movies = JSON.parse(xhr.responseText);
+        // for each movie add the movie to the moviesListcontainer
         movies.forEach(movie => {
             let moviesListContainer = document.createElement("div");
             moviesListContainer.innerText = movie.title;
             moviesListContainer.style.color = "#387EF5";
+            // append the movie the movies container
             output.appendChild(moviesListContainer);
         });  
 
     })
 
+    // open an HTTP request with get and pass the url
     xhr.open("GET", url);
+    // send the http request
     xhr.send();
 
 }
