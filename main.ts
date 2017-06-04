f124f677c41ca062c2929f1f68c2006c338846f1
chandanch <chandan.cec107@gmail.com>
2017-06-04 21:01:48 +0530

    using flatmap to subscribe to an inner observable

--- main.ts
+++ main.ts
@@ -16,41 +16,57 @@ let source = Observable.fromEvent(getMoviesBtn, "click");
  * @desc Get the data by making HTTP request
  * @param url
  */
-function getMovies(url : string) {
-    // Create an XHMLHTTPRequest object
-    let xhr = new XMLHttpRequest();
+function getData(url : string) {
+   return Observable.create(observer => {
+        // Create an XHMLHTTPRequest object
+        let xhr = new XMLHttpRequest();
+        // event handler for handling the response from the server
+        xhr.addEventListener("load", () => {
+            // parse the response and add to the movies
+            let movies = JSON.parse(xhr.responseText);
+            observer.next(movies);
+            observer.complete();
+        })
 
-    // event handler for handling the response from the server
-    xhr.addEventListener("load", () => {
-        // parse the response and add to the movies
-        let movies = JSON.parse(xhr.responseText);
-        // for each movie add the movie to the moviesListcontainer
-        movies.forEach(movie => {
-            let moviesListContainer = document.createElement("div");
-            moviesListContainer.innerText = movie.title;
-            moviesListContainer.style.color = "#387EF5";
-            // append the movie the movies container
-            moviesContainer.appendChild(moviesListContainer);
-        });  
-
+        // open an HTTP request with get and pass the url
+        xhr.open("GET", url);
+        // send the http request
+        xhr.send();
     })
+}
 
-    // open an HTTP request with get and pass the url
-    xhr.open("GET", url);
-    // send the http request
-    xhr.send();
-
+function renderMovies(movies) {
+    // for each movie add the movie to the moviesListcontainer
+    movies.forEach(movie => {
+        let moviesListContainer = document.createElement("div");
+        moviesListContainer.innerText = movie.title;
+        moviesListContainer.style.color = "#387EF5";
+        // append the movie the movies container
+        moviesContainer.appendChild(moviesListContainer);
+    });
 }
 
 // an observer which subscribe to the observable
-source.subscribe(
-    buttonClickevent => {
-        getMovies('movies.json');
-    },
-    error => {
-        console.log(`Error: ${error}`)
-    },
-    () => {
-        console.log("Completed");
-    }
-)
+source.flatMap(buttonClickEvent => getData("movies.json"))
+      .subscribe(
+          renderMovies,
+          error => {
+              console.log(`Error: ${error}`)
+          },
+          () => {
+              console.log("Completed");
+          }
+      )
+
+// source.subscribe(
+//     buttonClickevent => {
+//         getMovies('movies.json');
+//     },
+//     error => {
+//         console.log(`Error: ${error}`)
+//     },
+//     () => {
+//         console.log("Completed");
+//     }
+// )
+
